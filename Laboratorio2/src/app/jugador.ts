0d675b735e096983b7040dc1f085bec811d4e433
Julio José Carpio Díaz <jjcarpio@url.edu.gt>
2018-09-17 16:17:00 -0600

    Segundo Commit

---
+++ Laboratorio2/src/app/jugador.ts
@@ -0,0 +1,10 @@
+export class Jugador{
+    id: number;
+    nombre: string;
+    jersey: number;
+    estatura: number;
+    peso: number;
+    fec_nacimiento: Date;
+    posicionF: string;
+    posicionB: string;
+}

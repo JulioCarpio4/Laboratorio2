53b7e73fbe59c322100dababccf584f1871912bc
Julio José Carpio Díaz <jjcarpio@url.edu.gt>
2018-09-18 17:05:09 -0600

    Se agregó el formulario para mostrar la información al editar jugador.

--- Laboratorio2/src/app/edicionjugador/edicionjugador.component.ts
+++ Laboratorio2/src/app/edicionjugador/edicionjugador.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { Jugador } from '../jugador';
 
 @Component({
   selector: 'app-edicionjugador',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EdicionjugadorComponent implements OnInit {
 
-  constructor() { }
+  jugadores = [];
+  selectedplayer: string; 
+  selectedvalue:string;
+
+  constructor() {
+
+      this.allStorage();
+      console.log(this.jugadores);
+   }
 
   ngOnInit() {
   }
 
+  allStorage() {
+
+    var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while ( i-- ) {
+      let id: number;
+      let name: string; 
+      let temporal: string; 
+      let pivot;
+
+      temporal = localStorage.getItem(keys[i]);
+      pivot = JSON.parse(temporal);
+
+      id = pivot.id;
+      name = pivot.name 
+
+      this.jugadores.push({"id": id, "name": name});
+    }
+  }
+
+  SelectOption(event: any){
+    
+    if (event.target.value == "Seleccione un jugador...")
+    {
+      this.selectedplayer = undefined;
+    }
+    else
+    {
+      this.selectedplayer = "Sí";
+    }
+  };
 }

dba44db00a825ecd5f87fdea9c10b1a5f12685db
unknown <julio_carpio4@hotmail.com>
2018-09-30 15:25:02 -0600

    Implementación módulos - eliminar y actualizar información de un jugador

--- Laboratorio2/src/app/edicionjugador/edicionjugador.component.ts
+++ Laboratorio2/src/app/edicionjugador/edicionjugador.component.ts
@@ -25,6 +25,16 @@ export class EdicionjugadorComponent implements OnInit {
   lpeso: string;
   lposB: string;
   lposF: string;
+  
+  atrapa: string;
+  batea: string;
+
+  BD: boolean;
+  BZ: boolean;
+  AD: boolean;
+  AZ: boolean;
+
+  seleccion: string;
 
   constructor(private playerService: PlayersService) {
     this.inicio();
@@ -50,6 +60,7 @@ export class EdicionjugadorComponent implements OnInit {
       this.selectedplayer = "Sí";
       let option = String(event.target.value).split('-');
       this.selectedvalue = this.playerService.getJugador(Number(option[0]));
+      
       this.lnombre = this.selectedvalue.nombre;
       this.lestatura = this.selectedvalue.estatura;
       this.ljersey = this.selectedvalue.jersey;
@@ -58,6 +69,7 @@ export class EdicionjugadorComponent implements OnInit {
       this.lpeso = this.selectedvalue.peso
       this.lposB = this.selectedvalue.posicionB;
       this.lposF = this.selectedvalue.posicionF;
+      this.chequeoBat(this.lposB);
     }
   };
 
@@ -70,5 +82,170 @@ export class EdicionjugadorComponent implements OnInit {
     this.selectedvalue.peso = "";
     this.selectedvalue.posicionB = "";
     this.selectedvalue.posicionF = "";
+    
+  }
+
+  chequeoBat(valor){
+    if (valor == "Ambidiestro")
+    {
+        this.BD = true;
+        this.BZ = true;
+    }
+
+    else if (valor == "Zurdo")
+    {
+      this.BZ = true;
+      this.BD = false;
+    }
+
+    else 
+    {
+      this.BD = true;
+      this.BZ = false;
+    }
+  }
+
+  checkValueAD(event: any){
+    if (event == "Derecho" )
+    {
+      if(this.atrapa == "Zurdo")
+      {
+        this.atrapa = "Ambidiestro";
+      }
+
+      else if (this.atrapa == "")
+      {
+        this.atrapa = "Derecho"
+      }
+      
+    }
+    else 
+    {
+      if (this.atrapa == "Ambidiestro")
+      {
+        this.atrapa = "Zurdo";
+      }
+      
+      else 
+      {
+        this.atrapa = "";
+      }
+    }
+
+    console.log(this.atrapa)
+  };
+
+  checkValueAZ(event: any){
+    if (event == "Zurdo" )
+    {
+      if(this.atrapa == "Derecho")
+      {
+        this.atrapa = "Ambidiestro";
+      }
+
+      else if (this.atrapa == "")
+      {
+        this.atrapa = "Zurdo"
+      }
+      
+    }
+    else 
+    {
+      if (this.atrapa == "Ambidiestro")
+      {
+        this.atrapa = "Derecho";
+      }
+      else 
+      {
+        this.atrapa = "";
+      }      
+    }
+    console.log(this.atrapa)
+  }
+
+  checkValueBD(event: any){
+    if (event == "Derecho" )
+    {
+      if(this.batea == "Zurdo")
+      {
+        this.batea = "Ambidiestro";
+      }
+
+      else if (this.batea == "")
+      {
+        this.batea = "Derecho"
+      }
+      
+    }
+    else 
+    {
+      if (this.batea == "Ambidiestro")
+      {
+        this.batea = "Zurdo";
+      }
+      
+      else 
+      {
+        this.batea = "";
+      }
+    }
+
+    console.log(this.batea)
+  };
+
+  checkValueBZ(event: any){
+    if (event == "Zurdo" )
+    {
+      if(this.batea == "Derecho")
+      {
+        this.batea = "Ambidiestro";
+      }
+
+      else if (this.batea == "")
+      {
+        this.batea = "Zurdo"
+      }
+      
+    }
+    else 
+    {
+      if (this.batea == "Ambidiestro")
+      {
+        this.batea = "Derecho";
+      }
+      else 
+      {
+        this.batea = "";
+      }      
+    }
+    console.log(this.batea)
+  }
+
+  guardarJugador(){
+
+    this.selectedvalue.nombre = this.lnombre ;
+    this.selectedvalue.estatura = this.lestatura;
+    this.selectedvalue.jersey = this.ljersey;
+    this.selectedvalue.fec_nacimiento;
+    this.selectedvalue.id = this.lid;
+    this.selectedvalue.peso = this.lpeso;
+    this.selectedvalue.posicionB = this.batea;
+    this.selectedvalue.posicionF = this.atrapa;
+
+    //Se actualiza el nuevo jugador. 
+    this.playerService.postJugador(this.lid, this.selectedvalue)
+    this.selectedplayer = undefined;
+    this.seleccion = "Seleccione un jugador...";
+  }
+
+  eliminarJugador(){
+
+    //Eliminar jugador
+    localStorage.removeItem(String(this.lid));
+    this.selectedplayer = undefined;
+    this.seleccion = "Seleccione un jugador...";this.selectedplayer = undefined;
+    this.seleccion = "Seleccione un jugador...";
+    //"Seleccionar jugador..." nuevamente y ocultar elementos. 
+
   }
 }
